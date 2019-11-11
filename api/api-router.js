58633648df75dd38b31a74226c7c8ee838d7266d
Bayron Puac <bayronpuac@gmail.com>
2019-11-11 11:36:04 -0600

    bcrypt

--- api/api-router.js
+++ api/api-router.js
@@ -1,4 +1,7 @@
 const router = require('express').Router();
+const bcrypt = require('bcryptjs');
+const salt = bcrypt.genSaltSync(10);
+const hash = bcrypt.hashSync("B4c0/\/", salt);
 
 const authRouter = require('../auth/auth-router.js');
 const usersRouter = require('../users/users-router.js');
@@ -9,5 +12,9 @@ router.use('/users', usersRouter);
 router.get('/', (req, res) => {
   res.json({ api: "It's alive" });
 });
+
+router.post('/hash', (req, res ) => {
+  let hash = bcrypt.hashSync('myPassword', 10);
+})
 
 module.exports = router;
